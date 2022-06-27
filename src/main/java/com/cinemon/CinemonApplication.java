9b330fa6ac3a0124628ce35cd5451356644406ea
Rick Meels <rick.meels@gmail.com>
2022-06-27 11:15:20 +0200

    Initial project creation

---
+++ src/main/java/com/cinemon/CinemonApplication.java
@@ -0,0 +1,13 @@
+package com.cinemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemonApplication.class, args);
+    }
+
+}
